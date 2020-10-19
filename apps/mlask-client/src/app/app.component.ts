31d8ee95b7d412fd8503d194ac64a7777d4f3292
vulpeso <vulpes.lis@gmail.com>
2020-10-20 01:40:09 +0200

    initial commit

---
+++ apps/mlask-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mlask-dev-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mlask-client';
+}
