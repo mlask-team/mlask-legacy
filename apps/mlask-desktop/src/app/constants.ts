c7f64334810d8ea9df6fe521e97ace7d400f9dec
vulpeso <vulpes.lis@gmail.com>
2020-12-09 09:13:15 +0100

    electron desktop app

---
+++ apps/mlask-desktop/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'mlask-client';
+export const electronAppName = 'mlask-desktop';
+export const updateServerUrl = 'https://deployment-server-url.com';         // TODO: insert your update server url here
